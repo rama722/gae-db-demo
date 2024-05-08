eb3001aa5b87623503b9f731cd27d570ca51da36
rama <ramanaiduguntupalli722@gmail.com>
2024-05-08 10:46:15 +0530

    gae cloud react and nodejs

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    async function getApiData() {
+      const response = await fetch(
+        process.env.REACT_APP_API_ADDRESS + "/db-demo"
+      );
+      const responseData = await response.json();
+      setData(responseData);
+    }
+    getApiData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Google App Engine Demo Application</h1>
+      <div>Below some hello world data will appear from your API:</div>
+      <div>{data != undefined && data.map((d) => <div>{d.content}</div>)}</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+let dbConfig = {
+  /* Notice! These are here for demo purposes. DO NOT COMMIT YOUR INFO to version control*/
+  client: "mysql",
+  connection: {
+    user: "aanvy",
+    password: "Aanvy@2022",
+    database: "gae-db-demo",
+  },
+};
+
+if (process.env.NODE_ENV == "production") {
+  dbConfig.connection.socketPath = process.env.GAE_DB_ADDRESS;
+} else {
+  dbConfig.connection.host = "127.0.0.1";
+}
+
+const knex = require("knex")(dbConfig);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/db-demo", async (req, res) => {
+  const result = await knex.select().table("exampletable");
+  res.json(result);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
